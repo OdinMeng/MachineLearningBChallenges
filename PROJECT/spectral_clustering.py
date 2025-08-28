93b0178525890ecf1f89f26caa87e24a0986b4c4
OdinMeng <mengdino384@gmail.com>
2025-08-28 21:45:30 +0200

    started project (notebook with preliminary stuff)

---
+++ PROJECT/spectral_clustering.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+class SpectralClustering:
+    def __init__(self):
+        pass 
+
+    def fit_predict(self, X, Y):
+        pass 
+
---
+++ PROJECT/svd_spectral_clustering.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+class SVD_SpectralClustering:
+    def __init__(self):
+        pass 
+
+    def fit_predict(self, X, Y):
+        pass 
+
